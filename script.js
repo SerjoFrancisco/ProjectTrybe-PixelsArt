5375b9552c77bf4f08adfadb7bf3c26167947a43
Sergio Francisco <spinheiro.lf@gmail.com>
2021-12-09 17:29:15 -0300

    criando linhas

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // gerador de cores retirado deste link https://css-tricks.com/snippets/javascript/random-hex-color/
 const pallet = document.getElementsByClassName('color');
-function changePallet() {
+window.onload = function changePallet() {
   for (let i = 1; i < pallet.length; i += 1) {
     pallet[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-}
+};
+
+const board = document.getElementById('pixel-board');
+const n = 5;
+function createLines() {
+  for (let i = 0; i < n; i += 1) {
+    board.appendChild(document.createElement('li'));
+  }
+};
